461a03f2541ce61cccf8a4a5c8538a7b55b25692
Yootooo <yokimaxwel@gmail.com>
2026-06-21 11:29:00 +0300

    refactor(core): upgrade to many-to-many relationship and update context

--- LuftbornMusic.Core/Entities/Song.cs
+++ LuftbornMusic.Core/Entities/Song.cs
@@ -6,5 +6,5 @@ public class Song
     public string Title { get; set; } = string.Empty;
     public string Artist { get; set; } = string.Empty;
     public TimeSpan Duration { get; set; }
-    public Guid PlaylistId { get; set; }
+    public ICollection<Playlist> Playlists { get; set; } = new List<Playlist>();
 }
